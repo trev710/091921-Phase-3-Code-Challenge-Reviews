87f23736abfdd87b5a8b2848eb2521b4c32e4c6c
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-09-10 12:03:38 -0600

    assignment deadline

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,19 @@
 class Product < ActiveRecord::Base
-    
+    has_many :reviews 
+    has_many :users, through: :reviews 
+
+    def leave_review(user, star_rating, comment)
+        Review.create(star_rating: star_rating, comment: comment, product_id: self.id, user_id: user.id)
+    end
+
+    def print_all_reviews 
+        self.reviews.map do |review|
+            review.print_review 
+        end
+    end
+
+    def average_rating
+       total_ratings = self.reviews.map(&:star_rating).sum()
+       total_ratings / self.reviews.length.to_f
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+
 class User < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :products, through: :reviews
+
+    def remove_reviews(product) 
+        self.reviews.map do |review|
+            if review.product_id == product.id 
+                review.destroy
+            end
+        end
+    end
+
+    def favorite_product 
+        self.products.sort_by {|product| product.reviews.map(&:star_rating)}
+    end
 end
